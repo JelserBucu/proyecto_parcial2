39f4145aa03c0c832b253ef8b7f6f2359eb739a1
JelserBucu <68365887+JelserBucu@users.noreply.github.com>
2021-09-23 02:09:55 -0600

    Update datos_conexion.php

--- proyectoParcial2/datos_conexion.php
+++ proyectoParcial2/datos_conexion.php
@@ -1,6 +1,6 @@
 <?php
 $db_host= "localhost";
-$db_usr = "usr_empresa";
-$db_pass = "Empres@123";
-$db_nombre = "db_empresa";
+$db_usr = "usr_parcial2";
+$db_pass = "parci@l123";
+$db_nombre = "db_productos";
 ?>
